d7d201dba797afeba215a5477acd97b249bc3685
Pablo Arce <pablo.arce.garcia@gmail.com>
2016-09-30 16:11:42 +0100

    Added Spring configuration file to enable async requests

---
+++ vcf-dumper/vcf-dumper-ws/src/main/java/embl/ebi/variation/eva/vcfdump/server/VcfDumperWSConfig.java
@@ -0,0 +1,12 @@
+package embl.ebi.variation.eva.vcfdump.server;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.scheduling.annotation.EnableAsync;
+
+/**
+ * Created by pagarcia on 30/09/2016.
+ */
+@Configuration
+@EnableAsync
+public class VcfDumperWSConfig {
+}
