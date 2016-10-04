b5523b03dfbbc6af8b01341b7ed3415e931151aa
Pablo Arce <pablo.arce.garcia@gmail.com>
2016-10-04 10:57:29 +0100

    Added asyncConfigurer for customizing the asyncronous dump from the WS

--- vcf-dumper/vcf-dumper-ws/src/main/java/embl/ebi/variation/eva/vcfdump/server/VcfDumperWSConfig.java
+++ vcf-dumper/vcf-dumper-ws/src/main/java/embl/ebi/variation/eva/vcfdump/server/VcfDumperWSConfig.java
@@ -1,12 +1,31 @@
 package embl.ebi.variation.eva.vcfdump.server;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.scheduling.annotation.AsyncConfigurerSupport;
 import org.springframework.scheduling.annotation.EnableAsync;
+import org.springframework.scheduling.annotation.EnableScheduling;
+import org.springframework.scheduling.concurrent.ThreadPoolTaskExecutor;
+
+import java.util.concurrent.Executor;
 
 /**
  * Created by pagarcia on 30/09/2016.
  */
 @Configuration
 @EnableAsync
-public class VcfDumperWSConfig {
+@EnableScheduling
+public class VcfDumperWSConfig extends AsyncConfigurerSupport {
+
+    @Override
+    @Bean
+    public Executor getAsyncExecutor() {
+        // TODO: override those default values, or choose a different executor
+        ThreadPoolTaskExecutor pool = new ThreadPoolTaskExecutor();
+        pool.setCorePoolSize(5);
+        pool.setMaxPoolSize(10);
+        pool.setWaitForTasksToCompleteOnShutdown(true);
+        return pool;
+    }
+
 }
