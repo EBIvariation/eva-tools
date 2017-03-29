b0e4028e39b8c93672aab6cac471fef6660acc9d
Tom Smith <tsmith@ebi.ac.uk>
2017-03-29 14:36:23 +0100

    Changed package name to match classes

--- vcf-dumper/vcf-dumper-lib/src/test/java/uk/ac/ebi/eva/vcfdump/chromosomewsclient/ChromosomeWsClientTest.java
+++ vcf-dumper/vcf-dumper-lib/src/test/java/uk/ac/ebi/eva/vcfdump/chromosomewsclient/ChromosomeWsClientTest.java
@@ -13,7 +13,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-package uk.ac.ebi.eva.vcfdump.cellbasewsclient;
+package uk.ac.ebi.eva.vcfdump.chromosomewsclient;
 
 import org.junit.Before;
 import org.junit.Test;
