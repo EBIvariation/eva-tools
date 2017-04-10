eaa5796244dadf052d08cf0fc451a28cbaf956cc
Pablo Arce <pablo.arce.garcia@gmail.com>
2017-04-10 16:02:21 +0100

    EVA-509 updated group id for opencga and biodata dependencies (#13)
    * Upgraded biodata and opencga versions in pom.xml
    * Changed groupid for dependencies
    * Changed install-dependencies.sh script to install our forks of opencga and biodata
    * Removed unused .travis.settings.yml file

--- install-dependencies.sh
+++ install-dependencies.sh
@@ -1,6 +1,11 @@
 #!/usr/bin/env sh
 
-git clone -b hotfix/0.5 https://github.com/opencb/opencga.git
-cd opencga
-mvn install -DskipTests --settings ../.travis.settings.xml
+git clone -b hotfix/0.4 https://github.com/EBIvariation/biodata.git
+cd biodata
+mvn install -DskipTests
+cd ..
 
+git clone -b hotfix/0.5 https://github.com/EBIVariation/opencga.git
+cd opencga
+mvn install -DskipTests
+cd ..
