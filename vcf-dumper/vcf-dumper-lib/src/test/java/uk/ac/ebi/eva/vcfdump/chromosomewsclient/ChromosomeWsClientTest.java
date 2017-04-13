cae5c44001edbf180b1266867fccbcae21308de8
Tom Smith <tsmith@ebi.ac.uk>
2017-04-13 12:06:03 +0100

    - Removed use of MockServerHelper in MockServerRule - Removed MockServerHelper

--- vcf-dumper/vcf-dumper-lib/src/test/java/uk/ac/ebi/eva/vcfdump/chromosomewsclient/ChromosomeWsClientTest.java
+++ vcf-dumper/vcf-dumper-lib/src/test/java/uk/ac/ebi/eva/vcfdump/chromosomewsclient/ChromosomeWsClientTest.java
@@ -16,10 +16,15 @@
 package uk.ac.ebi.eva.vcfdump.chromosomewsclient;
 
 import org.junit.Before;
+import org.junit.Rule;
 import org.junit.Test;
 import org.mockserver.client.server.MockServerClient;
-
-import uk.ac.ebi.eva.vcfdump.MockServerHelper;
+import org.mockserver.junit.MockServerRule;
+import org.mockserver.model.HttpRequest;
+import org.mockserver.model.HttpResponse;
+import org.mockserver.model.JsonBody;
+import org.mockserver.model.Parameter;
+import org.springframework.http.HttpStatus;
 
 import java.util.Arrays;
 import java.util.HashSet;
@@ -28,17 +33,34 @@ import static org.junit.Assert.assertEquals;
 
 public class ChromosomeWsClientTest {
 
-    private MockServerClient mockServer;
+    @Rule
+    public MockServerRule mockServerRule = new MockServerRule(this);
+
+    private MockServerClient mockServerClient;
 
     @Before
     public void setUp() {
-        mockServer = MockServerHelper.getMockServer();
+        mockServerClient
+                .when(
+                        HttpRequest.request()
+                                   .withMethod("GET")
+                                   .withPath("/eva/webservices/rest/v1/segments")
+                                   .withQueryStringParameter(
+                                           new Parameter("species", "hsapiens_grch37")
+                                   )
+                )
+                .respond(
+                        HttpResponse.response()
+                                    .withHeader("Content-Type", "application/json", "charset=UTF-8")
+                                    .withStatusCode(HttpStatus.OK.value())
+                                    .withBody( new JsonBody("{\"apiVersion\":\"v1\",\"warning\":\"\",\"error\":\"\",\"response\":[{\"id\":\"\",\"time\":0,\"dbTime\":-1,\"numResults\":25,\"numTotalResults\":25,\"warningMsg\":\"\",\"errorMsg\":\"\",\"resultType\":\"\",\"result\":[\"1\",\"10\",\"11\",\"12\",\"13\",\"14\",\"15\",\"16\",\"17\",\"18\",\"19\",\"2\",\"20\",\"21\",\"22\",\"3\",\"4\",\"5\",\"6\",\"7\",\"8\",\"9\",\"MT\",\"X\",\"Y\"]}]}"))
+                );
     }
 
     @Test
     public void getChromosomes() throws Exception {
         ChromosomeWsClient chromosomeWsClient = new ChromosomeWsClient("eva_hsapiens_grch37",
-                                                                       "http://localhost:1080/eva/webservices/rest/",
+                                                                       String.format("http://localhost:%s/eva/webservices/rest/", mockServerRule.getPort()),
                                                                        "v1");
 
         assertEquals(
