b6719506c716c477e34f767b286da7374a599395
jose miguel mut <jomutlo@gmail.com>
2017-09-11 10:57:33 +0100

    query: remove ctg_id and use constants for column names

--- dbsnp-importer/src/main/java/uk/ac/ebi/eva/dbsnpimporter/io/readers/SubSnpCoreFieldsReader.java
+++ dbsnp-importer/src/main/java/uk/ac/ebi/eva/dbsnpimporter/io/readers/SubSnpCoreFieldsReader.java
@@ -26,12 +26,22 @@ import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
+import static uk.ac.ebi.eva.dbsnpimporter.io.readers.SubSnpCoreFieldsRowMapper.CHROMOSOME_COLUMN;
+import static uk.ac.ebi.eva.dbsnpimporter.io.readers.SubSnpCoreFieldsRowMapper.CHROMOSOME_END_COLUMN;
+import static uk.ac.ebi.eva.dbsnpimporter.io.readers.SubSnpCoreFieldsRowMapper.CHROMOSOME_START_COLUMN;
+import static uk.ac.ebi.eva.dbsnpimporter.io.readers.SubSnpCoreFieldsRowMapper.CONTIG_END_COLUMN;
+import static uk.ac.ebi.eva.dbsnpimporter.io.readers.SubSnpCoreFieldsRowMapper.CONTIG_NAME_COLUMN;
+import static uk.ac.ebi.eva.dbsnpimporter.io.readers.SubSnpCoreFieldsRowMapper.CONTIG_ORIENTATION_COLUMN;
+import static uk.ac.ebi.eva.dbsnpimporter.io.readers.SubSnpCoreFieldsRowMapper.CONTIG_START_COLUMN;
+import static uk.ac.ebi.eva.dbsnpimporter.io.readers.SubSnpCoreFieldsRowMapper.REFSNP_ID_COLUMN;
+import static uk.ac.ebi.eva.dbsnpimporter.io.readers.SubSnpCoreFieldsRowMapper.SNP_ORIENTATION_COLUMN;
+import static uk.ac.ebi.eva.dbsnpimporter.io.readers.SubSnpCoreFieldsRowMapper.SUBSNP_ID_COLUMN;
+
 /**
      SELECT
          sub.subsnp_id AS ss_id,
          loc.snp_id AS rs_id,
          ctg.contig_name AS contig_name,
-         ctg.contig_gi AS contig_id,
          loc.asn_from +1 AS contig_start,
          loc.asn_to +1 as contig_end,
          ctg.contig_chr AS chromosome,
@@ -51,11 +61,11 @@ import java.util.Map;
      WHERE
          ctg.group_term IN($extract_mappings_for)
          AND ctg.group_label LIKE '$group_label'
-     ORDER BY sorting_id ASC;
+     ORDER BY ss_id ASC;
  */
 public class SubSnpCoreFieldsReader extends JdbcPagingItemReader<SubSnpCoreFields> {
 
-    public SubSnpCoreFieldsReader(String assembly, List<String> assemblyType, DataSource dataSource, int pageSize)
+    public SubSnpCoreFieldsReader(String assembly, List<String> assemblyTypes, DataSource dataSource, int pageSize)
             throws Exception {
         if (pageSize < 1) {
             throw new IllegalArgumentException("Page size must be greater than zero");
@@ -67,7 +77,7 @@ public class SubSnpCoreFieldsReader extends JdbcPagingItemReader<SubSnpCoreField
         setPageSize(pageSize);
 
         Map<String, Object> parameterValues = new HashMap<>();
-        parameterValues.put("assemblyType", String.join(", ", assemblyType));
+        parameterValues.put("assemblyType", String.join(", ", assemblyTypes));
         parameterValues.put("assembly", assembly);
         setParameterValues(parameterValues);
     }
@@ -77,35 +87,34 @@ public class SubSnpCoreFieldsReader extends JdbcPagingItemReader<SubSnpCoreField
         factoryBean.setDataSource(dataSource);
         factoryBean.setSelectClause(
                 "SELECT " +
-                        "sub.subsnp_id AS ss_id, " +
-                        "loc.snp_id AS rs_id, " +
-                        "ctg.contig_name AS contig_name, " +
-                        "ctg.contig_gi AS contig_id, " +
-                        "loc.asn_from +1 AS contig_start, " +
-                        "loc.asn_to +1 as contig_end, " +
-                        "ctg.contig_chr AS chromosome, " +
-                        "loc.phys_pos_from + 1 AS chromosome_start, " +
-                        "loc.phys_pos_from + 1 + loc.asn_to - loc.asn_from AS chromosome_end, " +
-                        "CASE " +
+                        "sub.subsnp_id AS " + SUBSNP_ID_COLUMN +
+                        ",loc.snp_id AS " + REFSNP_ID_COLUMN +
+                        ",ctg.contig_name AS " + CONTIG_NAME_COLUMN +
+                        ",loc.asn_from +1 AS " + CONTIG_START_COLUMN +
+                        ",loc.asn_to +1 AS " + CONTIG_END_COLUMN +
+                        ",ctg.contig_chr AS " + CHROMOSOME_COLUMN +
+                        ",loc.phys_pos_from + 1 AS " + CHROMOSOME_START_COLUMN +
+                        ",loc.phys_pos_from + 1 + loc.asn_to - loc.asn_from AS " + CHROMOSOME_END_COLUMN +
+                        ",CASE " +
                         "   WHEN loc.orientation = 1 THEN -1 ELSE 1 " +
-                        "END AS snp_orientation, " +
-                        "CASE " +
+                        "END AS " + SNP_ORIENTATION_COLUMN +
+                        ",CASE " +
                         "   WHEN ctg.orient = 1 THEN -1 ELSE 1 " +
-                        "END AS contig_orientation"
+                        "END AS " + CONTIG_ORIENTATION_COLUMN
         );
         factoryBean.setFromClause(
-                "FROM" +
-                        "   b148_snpcontigloc loc JOIN " +
-                        "   b148_contiginfo ctg ON ctg.ctg_id = loc.ctg_id JOIN " +
-                        "   snpsubsnplink link ON loc.snp_id = link.snp_id JOIN " +
-                        "   subsnp sub ON link.subsnp_id = sub.subsnp_id"
+                "FROM " +
+                        "b148_snpcontigloc loc JOIN " +
+                        "b148_contiginfo ctg ON ctg.ctg_id = loc.ctg_id JOIN " +
+                        "snpsubsnplink link ON loc.snp_id = link.snp_id JOIN " +
+                        "subsnp sub ON link.subsnp_id = sub.subsnp_id "
         );
         factoryBean.setWhereClause(
                 "WHERE " +
-                        "   ctg.group_term IN (:assemblyType) AND " +
-                        "   ctg.group_label LIKE :assembly"
+                        "ctg.group_term IN (:assemblyType) AND " +
+                        "ctg.group_label LIKE :assembly"
         );
-        factoryBean.setSortKey("ss_id");
+        factoryBean.setSortKey(SUBSNP_ID_COLUMN);
 
         return factoryBean.getObject();
     }
