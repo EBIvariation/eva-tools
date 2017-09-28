fbe25804217de26e8bde6c8059ba27cf0dae4f43
Pablo Arce <pablo.arce.garcia@gmail.com>
2017-09-28 16:13:18 +0100

    Extracted coordinate translator (from dbsnp to EVA model) to a new class

--- dbsnp-importer/src/main/java/uk/ac/ebi/eva/dbsnpimporter/jobs/steps/processors/SubSnpCoreFieldsToVariantProcessor.java
+++ dbsnp-importer/src/main/java/uk/ac/ebi/eva/dbsnpimporter/jobs/steps/processors/SubSnpCoreFieldsToVariantProcessor.java
@@ -20,35 +20,27 @@ import org.springframework.batch.item.ItemProcessor;
 import uk.ac.ebi.eva.commons.core.models.IVariant;
 import uk.ac.ebi.eva.commons.core.models.Region;
 import uk.ac.ebi.eva.commons.core.models.pipeline.Variant;
-import uk.ac.ebi.eva.dbsnpimporter.models.LocationType;
 import uk.ac.ebi.eva.dbsnpimporter.models.SubSnpCoreFields;
 
 public class SubSnpCoreFieldsToVariantProcessor implements ItemProcessor<SubSnpCoreFields, IVariant> {
+
+    private VariantCoordinateTranslator coordinateTranslator;
+
+    public SubSnpCoreFieldsToVariantProcessor() {
+        coordinateTranslator = new VariantCoordinateTranslator();
+    }
 
     @Override
     public IVariant process(SubSnpCoreFields subSnpCoreFields) throws Exception {
         String referenceAllele = subSnpCoreFields.getReferenceInForwardStrand();
         String alternateAllele = subSnpCoreFields.getAlternateInForwardStrand();
 
-        Region chromosomeRegion = getVariantRegion(subSnpCoreFields);
+        Region chromosomeRegion = coordinateTranslator.getVariantRegion(subSnpCoreFields);
 
         IVariant variant = new Variant(chromosomeRegion.getChromosome(), chromosomeRegion.getStart(),
                                        chromosomeRegion.getEnd(), null, null);
         return variant;
     }
 
-    private Region getVariantRegion(SubSnpCoreFields subSnpCoreFields) {
-        Region variantRegion = subSnpCoreFields.getChromosomeRegion();
-        if (variantRegion == null) {
-            variantRegion = subSnpCoreFields.getContigRegion();
-        }
 
-        // adjust start for insertions
-        if (subSnpCoreFields.getLocationType().equals(LocationType.INSERTION)) {
-            variantRegion.setStart(variantRegion.getStart() + 1);
-            variantRegion.setEnd(variantRegion.getEnd() + subSnpCoreFields.getAlternate().length() - 1);
-        }
-
-        return variantRegion != null ? variantRegion : subSnpCoreFields.getContigRegion();
-    }
 }
