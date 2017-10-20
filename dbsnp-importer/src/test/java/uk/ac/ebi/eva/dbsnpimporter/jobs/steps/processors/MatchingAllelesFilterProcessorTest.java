a7e57ce7df3e26456fb4a9df20852df784178cb4
Pablo Arce <pablo.arce.garcia@gmail.com>
2017-10-20 15:09:32 +0100

    Fixed method call alignment

--- dbsnp-importer/src/test/java/uk/ac/ebi/eva/dbsnpimporter/jobs/steps/processors/MatchingAllelesFilterProcessorTest.java
+++ dbsnp-importer/src/test/java/uk/ac/ebi/eva/dbsnpimporter/jobs/steps/processors/MatchingAllelesFilterProcessorTest.java
@@ -28,56 +28,56 @@ public class MatchingAllelesFilterProcessorTest {
     public void setUp() throws Exception {
         filter = new MatchingAllelesFilterProcessor();
         matchingAllelesVariants = new ArrayList<>();
-        matchingAllelesVariants
-                .add(new SubSnpCoreFields(26201546L, Orientation.FORWARD, 13677177L, Orientation.FORWARD, "NT_455866.1",
-                                          1766472L, 1766472L, Orientation.FORWARD, LocusType.SNP, "4", 91223961L,
-                                          91223961L, "T", "T", "A", "T/A", "NC_006091.4:g.91223961T>A", 91223961L,
-                                          91223961L, Orientation.FORWARD, "NT_455866.1:g.1766472T>A", 1766472L,
-                                          1766472L, Orientation.FORWARD));
-        matchingAllelesVariants
-                .add(new SubSnpCoreFields(26954817L, Orientation.REVERSE, 13677177L, Orientation.FORWARD, "NT_455866.1",
-                                          1766472L, 1766472L, Orientation.FORWARD, LocusType.SNP, "4", 91223961L,
-                                          91223961L, "T", "T", "C", "G/A", "NC_006091.4:g.91223961T>C", 91223961L,
-                                          91223961L, Orientation.FORWARD, "NT_455866.1:g.1766472T>C", 1766472L,
-                                          1766472L, Orientation.FORWARD));
-        matchingAllelesVariants
-                .add(new SubSnpCoreFields(26963037L, Orientation.FORWARD, 13677177L, Orientation.FORWARD, "NT_455866.1",
-                                          1766472L, 1766472L, Orientation.FORWARD, LocusType.SNP, "4", 91223961L,
-                                          91223961L, "T", "T", "A", "T/A", "NC_006091.4:g.91223961T>A", 91223961L,
-                                          91223961L, Orientation.FORWARD, "NT_455866.1:g.1766472T>A", 1766472L,
-                                          1766472L, Orientation.FORWARD));
-        matchingAllelesVariants.add(new SubSnpCoreFields(0, Orientation.REVERSE, 0L, Orientation.FORWARD, "", 0L, 0L,
-                                                         Orientation.FORWARD, LocusType.SNP, "4", 0L, 0L, "T", "T", "C",
-                                                         "T/A/G", "", 0L, 0L, Orientation.FORWARD, "", 0L, 0L,
-                                                         Orientation.FORWARD));
-        matchingAllelesVariants.add(new SubSnpCoreFields(0, Orientation.REVERSE, 0L, Orientation.FORWARD, "", 0L, 0L,
-                                                         Orientation.FORWARD, LocusType.SNP, "4", 0L, 0L, "AT", "AT",
-                                                         "TGG", "TT/AT/CCA", "", 0L, 0L, Orientation.FORWARD, "", 0L,
-                                                         0L, Orientation.FORWARD));
+        matchingAllelesVariants.add(
+                new SubSnpCoreFields(26201546L, Orientation.FORWARD, 13677177L, Orientation.FORWARD, "NT_455866.1",
+                                     1766472L, 1766472L, Orientation.FORWARD, LocusType.SNP, "4", 91223961L,
+                                     91223961L, "T", "T", "A", "T/A", "NC_006091.4:g.91223961T>A", 91223961L,
+                                     91223961L, Orientation.FORWARD, "NT_455866.1:g.1766472T>A", 1766472L,
+                                     1766472L, Orientation.FORWARD));
+        matchingAllelesVariants.add(
+                new SubSnpCoreFields(26954817L, Orientation.REVERSE, 13677177L, Orientation.FORWARD, "NT_455866.1",
+                                     1766472L, 1766472L, Orientation.FORWARD, LocusType.SNP, "4", 91223961L,
+                                     91223961L, "T", "T", "C", "G/A", "NC_006091.4:g.91223961T>C", 91223961L,
+                                     91223961L, Orientation.FORWARD, "NT_455866.1:g.1766472T>C", 1766472L,
+                                     1766472L, Orientation.FORWARD));
+        matchingAllelesVariants.add(
+                new SubSnpCoreFields(26963037L, Orientation.FORWARD, 13677177L, Orientation.FORWARD, "NT_455866.1",
+                                     1766472L, 1766472L, Orientation.FORWARD, LocusType.SNP, "4", 91223961L,
+                                     91223961L, "T", "T", "A", "T/A", "NC_006091.4:g.91223961T>A", 91223961L,
+                                     91223961L, Orientation.FORWARD, "NT_455866.1:g.1766472T>A", 1766472L,
+                                     1766472L, Orientation.FORWARD));
+        matchingAllelesVariants.add(
+                new SubSnpCoreFields(0, Orientation.REVERSE, 0L, Orientation.FORWARD, "", 0L, 0L, Orientation.FORWARD,
+                                     LocusType.SNP, "4", 0L, 0L, "T", "T", "C", "T/A/G", "", 0L, 0L,
+                                     Orientation.FORWARD, "", 0L, 0L, Orientation.FORWARD));
+        matchingAllelesVariants.add(
+                new SubSnpCoreFields(0, Orientation.REVERSE, 0L, Orientation.FORWARD, "", 0L, 0L, Orientation.FORWARD,
+                                     LocusType.SNP, "4", 0L, 0L, "AT", "AT", "TGG", "TT/AT/CCA", "", 0L, 0L,
+                                     Orientation.FORWARD, "", 0L, 0L, Orientation.FORWARD));
 
         mismatchingAllelesVariants = new ArrayList<>();
-        mismatchingAllelesVariants
-                .add(new SubSnpCoreFields(26201546, Orientation.FORWARD, 13677177L, Orientation.FORWARD, "NT_455866.1",
-                                          1766472L, 1766472L, Orientation.FORWARD, LocusType.SNP, "4", 91223961L,
-                                          91223961L, "T", "T", "C", "T/A", "NC_006091.4:g.91223961T>C", 91223961L,
-                                          91223961L, Orientation.FORWARD, "NT_455866.1:g.1766472T>C", 1766472L,
-                                          1766472L, Orientation.FORWARD));
-        mismatchingAllelesVariants
-                .add(new SubSnpCoreFields(26954817, Orientation.REVERSE, 13677177L, Orientation.FORWARD, "NT_455866.1",
-                                          1766472L, 1766472L, Orientation.FORWARD, LocusType.SNP, "4", 91223961L,
-                                          91223961L, "T", "T", "A", "G/A", "NC_006091.4:g.91223961T>A", 91223961L,
-                                          91223961L, Orientation.FORWARD, "NT_455866.1:g.1766472T>A", 1766472L,
-                                          1766472L, Orientation.FORWARD));
-        mismatchingAllelesVariants
-                .add(new SubSnpCoreFields(26963037, Orientation.FORWARD, 13677177L, Orientation.FORWARD, "NT_455866.1",
-                                          1766472L, 1766472L, Orientation.FORWARD, LocusType.SNP, "4", 91223961L,
-                                          91223961L, "T", "T", "C", "T/A", "NC_006091.4:g.91223961T>C", 91223961L,
-                                          91223961L, Orientation.FORWARD, "NT_455866.1:g.1766472T>C", 1766472L,
-                                          1766472L, Orientation.FORWARD));
-        mismatchingAllelesVariants.add(new SubSnpCoreFields(0, Orientation.FORWARD, 0L, Orientation.FORWARD, "", 0L, 0L,
-                                                            Orientation.FORWARD, LocusType.SNP, "4", 0L, 0L, "T", "T",
-                                                            "C", "T/A/G", "", 0L, 0L, Orientation.FORWARD, "", 0L, 0L,
-                                                            Orientation.FORWARD));
+        mismatchingAllelesVariants.add(
+                new SubSnpCoreFields(26201546, Orientation.FORWARD, 13677177L, Orientation.FORWARD, "NT_455866.1",
+                                     1766472L, 1766472L, Orientation.FORWARD, LocusType.SNP, "4", 91223961L, 91223961L,
+                                     "T", "T", "C", "T/A", "NC_006091.4:g.91223961T>C", 91223961L, 91223961L,
+                                     Orientation.FORWARD, "NT_455866.1:g.1766472T>C", 1766472L, 1766472L,
+                                     Orientation.FORWARD));
+        mismatchingAllelesVariants.add(
+                new SubSnpCoreFields(26954817, Orientation.REVERSE, 13677177L, Orientation.FORWARD, "NT_455866.1",
+                                     1766472L, 1766472L, Orientation.FORWARD, LocusType.SNP, "4", 91223961L, 91223961L,
+                                     "T", "T", "A", "G/A", "NC_006091.4:g.91223961T>A", 91223961L, 91223961L,
+                                     Orientation.FORWARD, "NT_455866.1:g.1766472T>A", 1766472L, 1766472L,
+                                     Orientation.FORWARD));
+        mismatchingAllelesVariants.add(
+                new SubSnpCoreFields(26963037, Orientation.FORWARD, 13677177L, Orientation.FORWARD, "NT_455866.1",
+                                     1766472L, 1766472L, Orientation.FORWARD, LocusType.SNP, "4", 91223961L, 91223961L,
+                                     "T", "T", "C", "T/A", "NC_006091.4:g.91223961T>C", 91223961L, 91223961L,
+                                     Orientation.FORWARD, "NT_455866.1:g.1766472T>C", 1766472L, 1766472L,
+                                     Orientation.FORWARD));
+        mismatchingAllelesVariants.add(
+                new SubSnpCoreFields(0, Orientation.FORWARD, 0L, Orientation.FORWARD, "", 0L, 0L, Orientation.FORWARD,
+                                     LocusType.SNP, "4", 0L, 0L, "T", "T", "C", "T/A/G", "", 0L, 0L,
+                                     Orientation.FORWARD, "", 0L, 0L, Orientation.FORWARD));
     }
 
     @Test
