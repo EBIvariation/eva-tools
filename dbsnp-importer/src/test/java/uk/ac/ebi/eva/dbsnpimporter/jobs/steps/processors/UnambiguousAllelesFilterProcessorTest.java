53734b9b63b43034bdaf4be87869400c196a5f82
Cristina Yenyxe Gonzalez Garcia <cyenyxe@ebi.ac.uk>
2017-11-17 19:35:34 +0000

    New filter for unambiguous alleles (without IUPAC codes)

---
+++ dbsnp-importer/src/test/java/uk/ac/ebi/eva/dbsnpimporter/jobs/steps/processors/UnambiguousAllelesFilterProcessorTest.java
@@ -0,0 +1,66 @@
+/*
+ * Copyright 2017 EMBL - European Bioinformatics Institute
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package uk.ac.ebi.eva.dbsnpimporter.jobs.steps.processors;
+
+import org.junit.Test;
+import uk.ac.ebi.eva.dbsnpimporter.models.LocusType;
+import uk.ac.ebi.eva.dbsnpimporter.models.Orientation;
+import uk.ac.ebi.eva.dbsnpimporter.models.SubSnpCoreFields;
+
+import static org.junit.Assert.assertNotNull;
+
+public class UnambiguousAllelesFilterProcessorTest {
+
+    private UnambiguousAllelesFilterProcessor filter = new UnambiguousAllelesFilterProcessor();
+
+    @Test
+    public void keepUnambiguousAlleles() {
+        SubSnpCoreFields subSnpCoreFields1 = new SubSnpCoreFields(1L, Orientation.FORWARD, 1L,
+                Orientation.FORWARD, "NT_455866.1",1L, 1L, Orientation.FORWARD, LocusType.SNP, "4",
+                1L,1L, "T", "T", "A", "T/A", "NC_006091.4:g.91223961T>A",null, null,
+                Orientation.FORWARD, null, null,null, Orientation.FORWARD, "batch");
+
+        SubSnpCoreFields subSnpCoreFields2 = new SubSnpCoreFields(1L, Orientation.FORWARD, 1L,
+                Orientation.FORWARD, "NT_455866.1",1L, 1L, Orientation.FORWARD, LocusType.SNP, "4",
+                1L,1L, "T", "TAGC", "A", "TAGC/A", "NC_006091.4:g.91223961T>A", null, null,
+                Orientation.FORWARD, null, null, null, Orientation.FORWARD, "batch");
+
+        SubSnpCoreFields subSnpCoreFields3 = new SubSnpCoreFields(1L, Orientation.FORWARD, 1L,
+                Orientation.FORWARD, "NT_455866.1",1L, 1L, Orientation.FORWARD, LocusType.SNP, "4",
+                1L,1L, "T", "aCgTAcGt", "AAACCTg", "aCgTAcGt/AAACCTg", "NC_006091.4:g.91223961T>A", null, null,
+                Orientation.FORWARD, null, null, null, Orientation.FORWARD, "batch");
+
+        SubSnpCoreFields subSnpCoreFields4 = new SubSnpCoreFields(1L, Orientation.FORWARD, 1L,
+                Orientation.FORWARD, "NT_455866.1",1L, 1L, Orientation.FORWARD, LocusType.SNP, "4",
+                1L,1L, "T", "", "ACgt", "/ACgt", "NC_006091.4:g.91223961T>A", null, null,
+                Orientation.FORWARD, null, null, null, Orientation.FORWARD, "batch");
+
+        assertNotNull(filter.process(subSnpCoreFields1));
+        assertNotNull(filter.process(subSnpCoreFields2));
+        assertNotNull(filter.process(subSnpCoreFields3));
+        assertNotNull(filter.process(subSnpCoreFields4));
+    }
+
+    @Test
+    public void removeAmbiguousReferenceAllele() {
+
+    }
+
+    @Test
+    public void removeAmbiguousAlternateAllele() {
+
+    }
+}
