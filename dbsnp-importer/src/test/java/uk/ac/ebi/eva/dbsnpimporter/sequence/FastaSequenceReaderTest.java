f43ef503029d57d7fb21956b0783103c42024afa
Pablo Arce <pablo.arce.garcia@gmail.com>
2017-12-01 14:15:49 +0000

    Added interface SequenceReader, class FastaSequence Reader and test (not working yet)

---
+++ dbsnp-importer/src/test/java/uk/ac/ebi/eva/dbsnpimporter/sequence/FastaSequenceReaderTest.java
@@ -0,0 +1,40 @@
+/*
+ * Copyright 2017 EMBL - European Bioinformatics Institute
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package uk.ac.ebi.eva.dbsnpimporter.sequence;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import java.nio.file.Paths;
+
+import static org.junit.Assert.*;
+
+public class FastaSequenceReaderTest {
+
+    private FastaSequenceReader reader;
+
+    @Before
+    public void setUp() throws Exception {
+        reader = new FastaSequenceReader(
+                Paths.get("src/test/resources/Gallus_gallus.Gallus_gallus-5.0.dna.chromosome.22.fa.gz"));
+    }
+
+    @Test
+    public void getFirstNucleotideOfContig() throws Exception {
+        assertEquals("N", reader.getSequence("22", 1, 1));
+    }
+}
