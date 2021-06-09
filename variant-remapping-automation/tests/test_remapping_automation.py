0dece73dc95f1c92fb7dec82ad191b793eab80f6
Timothee Cezard <tcezard@ebi.ac.uk>
2021-06-09 16:00:03 +0100

    Update variant-remapping-automation/tests/test_remapping_automation.py
    Co-authored-by: April Shen <april.tuesday@gmail.com>

--- variant-remapping-automation/tests/test_remapping_automation.py
+++ variant-remapping-automation/tests/test_remapping_automation.py
@@ -4,9 +4,8 @@ import unittest
 from remapping_automation import count_variants_extracted
 
 
-class TestRemmapingAutomation(unittest.TestCase):
+class TestRemappingAutomation(unittest.TestCase):
 
     def test_count_variants_extracted(self):
         log_file = os.path.abspath(os.path.join(os.path.dirname(__file__), 'ressources', 'vcf_extractor.log'))
         assert count_variants_extracted(log_file) == (25434599, 25434599, 72446279, 72446277)
-
